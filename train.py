4b0366853ce350722096fa477f6f53a141c43035
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2021-04-14 12:01:24 +0100

    Bug fix

--- train.py
+++ train.py
@@ -113,21 +113,24 @@ def train():
 
 
 def check_if_target_bbox_degenerate(targets):
-    if targets is None:
-        for target_idx, target in enumerate(targets):
-            boxes = target["boxes"]
-            degenerate_boxes = None
-            if len(boxes) == 4:
-                return True
 
-            degenerate_boxes = boxes[:, 2:] <= boxes[:, :2]
-            if degenerate_boxes.any():
-                # print the first degenerate box
-                bb_idx = T.where(degenerate_boxes.any(dim=1))[0][0]
-                degen_bb = boxes[bb_idx].tolist()
-                print("All bounding boxes should have positive height and width.")
-                print(f"Found invalid box {degen_bb} for target at index {target_idx}.")
-                return True
+    if targets is None:
+        return False
+
+    for target_idx, target in enumerate(targets):
+        boxes = target["boxes"]
+        degenerate_boxes = None
+        if len(boxes.shape) != 2:
+            return True
+
+        degenerate_boxes = boxes[:, 2:] <= boxes[:, :2]
+        if degenerate_boxes.any():
+            # print the first degenerate box
+            bb_idx = T.where(degenerate_boxes.any(dim=1))[0][0]
+            degen_bb = boxes[bb_idx].tolist()
+            print("All bounding boxes should have positive height and width.")
+            print(f"Found invalid box {degen_bb} for target at index {target_idx}.")
+            return True
     return False
 
 
